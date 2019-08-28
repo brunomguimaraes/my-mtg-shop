e371843e6a88c26194fee849326a3b85b59facf5
bruno.guimaraes <bruno.guimaraes@cubos.io>
2019-08-28 20:40:54 -0300

    Theme Added

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,51 +1,32 @@
 import React from "react";
 import "./App.css";
-import environment from "../../Environment";
-import { QueryRenderer } from "react-relay";
-import { graphql } from "babel-plugin-relay/macro";
 import NavBar from "../../components/NavBar";
-import { AppQuery } from "./__generated__/AppQuery.graphql";
+import Products from "../Products";
+import { ThemeProvider } from "@material-ui/styles";
+import { createMuiTheme } from "@material-ui/core";
+
+const myMtgShopTheme = createMuiTheme({
+    palette: {
+        primary: {
+            main: "#252627"
+        },
+        secondary: {
+            main: "#4B88A2",
+            light: "#A3D5FF"
+        },
+        error: {
+            main: "#BB0A21"
+        }
+    }
+});
 
 const App: React.FC = () => {
     return (
         <React.Fragment>
-            <NavBar />
-            <QueryRenderer<AppQuery>
-                environment={environment}
-                query={graphql`
-                    query AppQuery {
-                        viewer {
-                            allProducts {
-                                count
-                                edges {
-                                    node {
-                                        name
-                                        price
-                                        id
-                                    }
-                                }
-                            }
-                        }
-                    }
-                `}
-                variables={{}}
-                render={({ error, props }): React.ReactNode => {
-                    if (error) {
-                        return <div>Error!</div>;
-                    }
-                    if (!props) {
-                        return <div>Loading...</div>;
-                    }
-                    console.log("response:", props);
-                    return (
-                        <div>
-                            {props.viewer.allProducts.edges!.map((e: any) => (
-                                <p key={e.node.id}>{e.node.name}</p>
-                            ))}
-                        </div>
-                    );
-                }}
-            />
+            <ThemeProvider theme={myMtgShopTheme}>
+                <NavBar />
+                <Products />
+            </ThemeProvider>
         </React.Fragment>
     );
 };
