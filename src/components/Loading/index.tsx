f255dcaa771699249d9beb549b5ab79c0ed36461
bruno.guimaraes <bruno.guimaraes@cubos.io>
2019-08-29 11:51:29 -0300

    Loading wrapper style

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -4,6 +4,13 @@ import CircularProgress from "@material-ui/core/CircularProgress";
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
+        wrapper: {
+            width: "100%",
+            height: "100vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center"
+        },
         progress: {
             margin: theme.spacing(2)
         }
@@ -14,7 +21,7 @@ export default function Loading() {
     const classes = useStyles();
 
     return (
-        <div>
+        <div className={classes.wrapper}>
             <CircularProgress className={classes.progress} />
         </div>
     );
