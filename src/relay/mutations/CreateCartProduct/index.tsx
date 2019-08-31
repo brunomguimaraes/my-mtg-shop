7b80881ee383f662f26b2b6003903673a1ecf636
bruno.guimaraes <bruno.guimaraes@cubos.io>
2019-08-31 17:38:55 -0300

    Moved And Renamed Mutation Files

--- src/relay/mutations/CreateCartProduct/index.tsx
+++ src/relay/mutations/CreateCartProduct/index.tsx
@@ -1,6 +1,6 @@
 import { graphql } from "babel-plugin-relay/macro";
 import { commitMutation } from "react-relay";
-import environment from "../Environment";
+import environment from "../../Environment";
 
 const mutation = graphql`
     mutation CreateCartProductMutation($input: CreateCartProductInput!) {
---
+++ src/relay/mutations/UpdateCartProduct/index.tsx
@@ -0,0 +1,57 @@
+import { graphql } from "babel-plugin-relay/macro";
+import { commitMutation } from "react-relay";
+import environment from "../../Environment";
+
+const mutation = graphql`
+    mutation CreateCartProductMutation($input: CreateCartProductInput!) {
+        createCartProduct(input: $input) {
+            clientMutationId
+            cartProduct {
+                quantityOnCart
+                id
+                product {
+                    name
+                }
+            }
+            shoppingCart {
+                cartProducts {
+                    count
+                    edges {
+                        node {
+                            quantityOnCart
+                            product {
+                                name
+                                price
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+`;
+
+export const createCartProduct = (
+    clientMutationId: string,
+    quantityOnCart: number,
+    productId: string,
+    shoppingCartId: string
+) => {
+    const variables = {
+        input: {
+            clientMutationId,
+            quantityOnCart,
+            productId,
+            shoppingCartId
+        }
+    };
+
+    commitMutation(environment, {
+        mutation,
+        variables,
+        onCompleted: (response, errors) => {
+            console.log("Response received from server.");
+        },
+        onError: (err) => console.error(err)
+    });
+};
