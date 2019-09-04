53b6cc5f3449bf4b8bfdef4fb0a41a06321e1062
Bruno <mguimaraesbruno@gmail.com>
2019-09-03 21:00:18 -0300

    syntax fix

--- src/relay/mutations/UpdateCartProduct/index.tsx
+++ src/relay/mutations/UpdateCartProduct/index.tsx
@@ -3,55 +3,53 @@ import { commitMutation } from "react-relay";
 import environment from "../../Environment";
 
 const mutation = graphql`
-    mutation UpdateCartProductMutation($input: UpdateCartProductInput!) {
-        updateCartProduct(input: $input) {
-            clientMutationId
-            cartProduct {
-                quantityOnCart
-                id
-                product {
-                    name
-                    quantityInStock
-                }
-            }
-            shoppingCart {
-                cartProducts {
-                    count
-                    edges {
-                        node {
-                            quantityOnCart
-                            product {
-                                name
-                                price
-                                quantityInStock
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
+	mutation UpdateCartProductMutation($input: UpdateCartProductInput!) {
+		updateCartProduct(input: $input) {
+			clientMutationId
+			cartProduct {
+				quantityOnCart
+				id
+				product {
+					name
+					quantityInStock
+				}
+			}
+			shoppingCart {
+				cartProducts {
+					count
+					edges {
+						node {
+							quantityOnCart
+							product {
+								name
+								price
+								quantityInStock
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 `;
 
 export const updateCartProduct = (
-    clientMutationId: string,
-    id: string,
-    quantityOnCart: number
+	clientMutationId: string,
+	id: string,
+	quantityOnCart: number
 ) => {
-    const variables = {
-        input: {
-            clientMutationId,
-            id,
-            quantityOnCart
-        }
-    };
+	const variables = {
+		input: {
+			clientMutationId,
+			id,
+			quantityOnCart
+		}
+	};
 
-    commitMutation(environment, {
-        mutation,
-        variables,
-        onCompleted: (response, errors) => {
-            console.log("Response received from server.");
-        },
-        onError: (err) => console.error(err)
-    });
+	commitMutation(environment, {
+		mutation,
+		variables,
+		onCompleted: (response, errors) => {},
+		onError: err => console.error(err)
+	});
 };
